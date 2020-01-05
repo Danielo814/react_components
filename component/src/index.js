ed5b8a564d3588ad0d6c2eb25d80527217d71acc
Danielo814 <ojedadaniel92@gmail.com>
2020-01-05 13:52:59 -0800

    added comment data and avatar properties

--- component/src/index.js
+++ component/src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        commentData="Wow! good read!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        commentData="Nice!"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        commentData="Love it!"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
