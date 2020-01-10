92bc8147612e30e72de3dac1ed9ad0c31bd3da1f
Danielo814 <ojedadaniel92@gmail.com>
2020-01-09 18:33:31 -0800

    add classes for styling

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,4 +1,10 @@
+import "./SeasonDisplay.css";
 import React from "react";
+
+const seasonConfig = {
+  summer: { text: "lets hit the beach!", iconName: "sun" },
+  winter: { text: "Burrr, it's chilly", iconName: "snowflake" }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -10,13 +16,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text =
-    season === "winter" ? "Burrr, it's chilly" : "lets hit the beach!";
-  const icon = season === "winter" ? "snowflake" : "sun";
+  const { text, iconName } = seasonConfig[season];
 
   return (
-    <div>
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
       <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
     </div>
   );
 };
