45a343be2f74fe4b96fc04f01ea6a3ff95a9c019
Danielo814 <ojedadaniel92@gmail.com>
2020-01-15 15:21:34 -0800

    make axios requests

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID ca9af99042873674ef7dd989a7e3cdf72c574c69a7f1bb4805fd030d0c365ddf"
+      }
+    });
   }
 
   render() {
