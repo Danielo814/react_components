d3ecc7bb740f7c77da3c3bf2f75668ef26c5eb0d
Danielo814 <ojedadaniel92@gmail.com>
2020-01-15 17:45:58 -0800

    get data structure returned from network call

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -4,13 +4,17 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID ca9af99042873674ef7dd989a7e3cdf72c574c69a7f1bb4805fd030d0c365ddf"
-      }
-    });
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID ca9af99042873674ef7dd989a7e3cdf72c574c69a7f1bb4805fd030d0c365ddf"
+        }
+      })
+      .then(response => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
