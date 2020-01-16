e2d21ecf6c5799e2de8f8136de07b5e1fefcbb39
Danielo814 <ojedadaniel92@gmail.com>
2020-01-15 17:48:04 -0800

    use async/await instead of .then

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,18 +3,15 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get("https://api.unsplash.com/search/photos", {
-        params: { query: term },
-        headers: {
-          Authorization:
-            "Client-ID ca9af99042873674ef7dd989a7e3cdf72c574c69a7f1bb4805fd030d0c365ddf"
-        }
-      })
-      .then(response => {
-        console.log(response.data.results);
-      });
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID ca9af99042873674ef7dd989a7e3cdf72c574c69a7f1bb4805fd030d0c365ddf"
+      }
+    });
+    console.log(response.data.results);
   }
 
   render() {
