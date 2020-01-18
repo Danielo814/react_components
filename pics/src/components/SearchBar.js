c21a8d19ce253a2bcde7c7f95588b55451c282cd
Danielo814 <ojedadaniel92@gmail.com>
2020-01-17 22:30:03 -0800

    update function

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,11 +3,10 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit = event => {
+  onFormSubmit() {
     event.preventDefault();
-
     this.props.onSubmit(this.state.term);
-  };
+  }
 
   render() {
     return (
