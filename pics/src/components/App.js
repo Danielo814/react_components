a5d6137d023642f32396a11247d8702d2f4bbd6a
Danielo814 <ojedadaniel92@gmail.com>
2020-01-21 17:47:48 -0800

    move axios base url to separate file to utilize axios.create baseUrl prop

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
-import axios from "axios";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
   onSearchSubmit = async term => {
-    const response = await axios.get("https://api.unsplash.com/search/photos", {
-      params: { query: term },
-      headers: {
-        Authorization:
-          "Client-ID ca9af99042873674ef7dd989a7e3cdf72c574c69a7f1bb4805fd030d0c365ddf"
-      }
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
     });
     this.setState({ images: response.data.results });
   };
