8d81650f75ef70c7ab4e2973312185ae6f813c17
Danielo814 <ojedadaniel92@gmail.com>
2020-01-23 15:36:58 -0800

    import videodetail component to get selected video title

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
   onVideoSelect = video => {
-    console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
   };
 
   onTermSubmit = async term => {
@@ -23,6 +24,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videos}
