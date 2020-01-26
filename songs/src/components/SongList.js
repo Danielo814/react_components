1cf4d6b58d03b5b6f91c43fd6c29a4d72a8bd00e
Danielo814 <ojedadaniel92@gmail.com>
2020-01-26 14:28:04 -0800

    add new classname to div in render method for better visual

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -16,7 +16,7 @@ class SongList extends React.Component {
   }
 
   render() {
-    return <div>{this.renderList()}</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
