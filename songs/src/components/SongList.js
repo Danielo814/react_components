176ccbdcc16ac55d6d96a3e556dbb9d68cf79954
Danielo814 <ojedadaniel92@gmail.com>
2020-01-26 13:48:30 -0800

    return object with list of songs

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -3,13 +3,12 @@ import { connect } from "react-redux";
 
 class SongList extends React.Component {
   render() {
-    return <div>Song List</div>;
+    return <div>SongList</div>;
   }
 }
 
 const mapStateToProps = state => {
-  console.log(state);
-  return state;
+  return { songs: state.songs };
 };
 
 export default connect(mapStateToProps)(SongList);
