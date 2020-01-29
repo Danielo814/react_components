3bf39ec34d10109bc7cc6d22c41c5fbb1444d131
Danielo814 <ojedadaniel92@gmail.com>
2020-01-28 18:06:58 -0800

    add title and description JSX

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -12,6 +12,12 @@ class PostList extends React.Component {
       return (
         <div className="item" key={post.id}>
           <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
         </div>
       );
     });
